b3470a93eefd8094dbc8de4e52732163e922d5d0
Taylor Zanke <taylor.zanke@gmail.com>
2022-01-26 17:28:53 -0800

    Deploy Next.js to gh-pages

---
+++ out/_next/static/fbQz9UFEz4npa5a4jWCU-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-72dfd888fdbf0d2c.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
