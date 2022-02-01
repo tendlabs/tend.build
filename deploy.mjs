303b600d8bf90de68d76a87adc381a9e7be82988
Eduardo Del Balso <e.delbalso@gmail.com>
2022-01-31 20:07:07 -0800

    Deploy script updates

---
+++ deploy.mjs
@@ -0,0 +1,31 @@
+#!/usr/bin/env zx
+$.verbose = false
+
+const step = async(msg, fn) => {
+  process.stdout.write(chalk.blue(`** ${msg} ...`))
+  const p = await fn()
+  // const spinner = chalkAnimation.radar('...')
+  // spinner.stop()
+  console.log(chalk.green(' done!'))
+  return p
+}
+
+await step('Switching to main branch', () => $`git checkout main`)
+await step('Building next.js site', () => $`npm run deploy:build`)
+const output = await step('Checking if local copy of deploy branch exists', () => nothrow($`git branch -a | grep gh-pages | grep -v remotes`))
+
+if(output.exitCode !== 1) {
+  console.log(chalk.yellow('  - Branch found, let\'s delete it'))
+  await step('Deleting gh-pages since it exists locally', () => $`git branch -D gh-pages`)
+} else {
+  console.log('  - No branch found, good to go!')
+}
+
+await step('Creating deploy branch named `gh-pages`', () => $`git checkout -b gh-pages`)
+await step('Adding jekyll ignore file', () => $`touch out/.nojekyll`)
+await step('git add --all', () => $`git add --all ./out -f`)
+await step('Committing build output', () => $`git commit -m ${'Deploy Next.js to gh-pages'}`)
+await step('Force pushing deploy branch', () => $`git push origin gh-pages -f`)
+await step('Switching back to main', () => $`git checkout main`)
+await step('Cleaning up: Deleting deploy branch', () => $`git branch -D gh-pages`)
+
