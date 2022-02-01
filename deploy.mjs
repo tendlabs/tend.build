ee6f279caf55302c1307ffe9fa190cdd6bd130c1
Eduardo Del Balso <e.delbalso@gmail.com>
2022-02-01 09:59:03 -0800

    Deploy fix

--- deploy.mjs
+++ deploy.mjs
@@ -22,6 +22,7 @@ const step = async(msg, fn) => {
 }
 
 await step('Switching to main branch', () => $`git checkout main`)
+await step('Clearing out local copies of site', () => $`rm -rf out ; rm -rf docs`)
 await step('Building next.js site', () => $`npm run deploy:build`)
 const output = await step('Checking if local copy of deploy branch exists', () => nothrow($`git branch -a | grep gh-pages | grep -v remotes`))
 
@@ -32,11 +33,9 @@ if(output.exitCode !== 1) {
   console.log('  - No branch found, good to go!')
 }
 
-await step('Clearing out local copies of site', () => $`rm -rf out ; rm -rf docs`)
-await step('Creating output folders', () => $`mkdir out ; mkdir docs`)
 await step('Creating deploy branch named `gh-pages`', () => $`git checkout -b gh-pages`)
 await step('Adding jekyll ignore file', () => $`touch out/.nojekyll`)
-await step('Copying `out` to `docs`', () => $`cp -R ./out/* ./docs/`)
+await step('Copying `out` to `docs`', () => $`mv out docs`)
 await step('git add --all', () => $`git add --all ./docs -f`)
 await step('Committing build output', () => $`git commit -m ${'Deploy Next.js to gh-pages'}`)
 await step('Force pushing deploy branch', () => $`git push origin gh-pages -f`)
