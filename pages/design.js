ff7fd2a05c5666b8f4a8421b001e0ac80ca0e242
Taylor Zanke <taylor.zanke@gmail.com>
2022-02-05 15:40:49 -0800

    parallax variant

--- pages/design.js
+++ pages/design.js
@@ -22,7 +22,7 @@ const Design = ({}) => {
   const [ scrollYState, setScrollY ] = useState(0)
   const { scrollY } = useViewportScroll()
   scrollY.onChange(setScrollY)
-  const y1 = useTransform(scrollY, [0, 2400], [48, -48]);
+  const y1 = useTransform(scrollY, [0, 3200], [120, -120]);
   const y2 = useTransform(scrollY, [0, 1000], [50, -50]);
 
   console.log(y1)
